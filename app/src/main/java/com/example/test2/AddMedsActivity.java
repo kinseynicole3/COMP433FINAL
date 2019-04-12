b3eea4dff2693b0c6db308295db6e1bfa240f40b
aegustaf <agustafson52@gmail.com>
2019-04-11 22:36:19 -0400

    User interface for Add Meds created

--- app/src/main/java/com/example/test2/AddMedsActivity.java
+++ app/src/main/java/com/example/test2/AddMedsActivity.java
@@ -2,12 +2,19 @@ package com.example.test2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class AddMeds extends AppCompatActivity {
+public class AddMedsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_meds);
+
+
+    }
+
+    public void save(View view) {
+        
     }
 }
