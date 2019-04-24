9bae658710d696688a87d95af5465b093ee3d29a
aegustaf <agustafson52@gmail.com>
2019-04-24 18:53:19 -0400

    yay

--- app/src/main/java/com/example/test2/AlertReceiver.java
+++ app/src/main/java/com/example/test2/AlertReceiver.java
@@ -5,14 +5,22 @@ import android.app.job.JobService;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Handler;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 public class AlertReceiver extends BroadcastReceiver {
+
+    public AlertReceiver() {
+
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
         NotificationHelper notificationHelper = new NotificationHelper(context);
-        NotificationCompat.Builder nb = notificationHelper.getChannelNotification("String 1", "String 2");
+        NotificationCompat.Builder nb = notificationHelper.getChannelNotification("Med Control Reminder", "Take your medication today!");
         notificationHelper.getManager().notify(1, nb.build());
     }
 }
